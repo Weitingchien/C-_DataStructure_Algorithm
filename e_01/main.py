c9aa46342a4f193ab0a6f6ca0672166e332d9ab2
Wei-Ting <tony123445s54@yahoo.com.tw>
2023-01-19 17:37:12 +0800

    Add pd

--- e_01/main.py
+++ e_01/main.py
@@ -1,6 +1,13 @@
-class main():
+import pandas as pd
+
+class main:
+    def __init__(self) -> None:
+        first_text_file = pd.read_csv('input001.txt', sep="\t")
+        print(first_text_file)
     pass
 
 
+
 if __name__ == '__main__':
-    main()
+    main = main()
+    # print(main)
